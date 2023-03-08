65b349ae61d7d7f4f75e24e8aa264aff05341d5c
kyunghwa <snoopn303@gmail.com>
2023-03-08 16:51:46 +0900

    JobScheduler

---
+++ app/src/main/java/sesac/part21/limit/JobScheduler/MainActivity.kt
@@ -0,0 +1,11 @@
+package sesac.part21.limit.JobScheduler
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
